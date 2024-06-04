4c703007a89655ee085b211e1b58d8eb83d959ff
Salahaldeen0saif <salahaldeensaifallah@gmail.com>
2024-06-05 01:42:11 +0300

    Update dailey.js

--- plugins/dailey.js
+++ plugins/dailey.js
@@ -1,11 +1,11 @@
 let handler = async (m, {command, text, conn, usedPrefix}) => {
 
-let url = 'https://api.whatsapp.com/send?phone=';
+let url = 'https://wa.me/+201147144354';
 let a7a = url + text;
 await conn.reply(m.chat, a7a, m);
 }
     handler.help = ['تبنيد'];
-    handler.tags = ['K U R O S A K I'];
+    handler.tags = ['group'];
     handler.command = /^(فنش)$/i
 
     export default handler;
