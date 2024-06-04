7fef058ec8a55890b27e3085fce1a1e44dbfb8b0
Salahaldeen0saif <salahaldeensaifallah@gmail.com>
2024-06-05 01:46:32 +0300

    Update kana.js

--- plugins/kana.js
+++ plugins/kana.js
@@ -3,9 +3,23 @@ const handler = (m) => m;
 handler.all = async function(m) {
   const vn = './media/zoro.mp3';
   const chat = global.db.data.chats[m.chat];
-  if (/^لوفي$/i.test(m.text) && !chat.isBanned) {
+  if (/^كايدو$/i.test(m.text) && !chat.isBanned) {
     conn.sendPresenceUpdate('recording', m.chat);
-    await m.reply(`*اهلا, ¿انا لوفي?*`);
+    await m.reply(`*رارارارار، عايز شنو؟!*`);
+    m.conn.sendMessage(m.chat, {audio: {url: vn}, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
+  }
+  return !0;
+};
+export default handler;
+
+import fs from 'fs';
+const handler = (m) => m;
+handler.all = async function(m) {
+  const vn = './media/ace.mp3';
+  const chat = global.db.data.chats[m.chat];
+  if (/^ايس$/i.test(m.text) && !chat.isBanned) {
+    conn.sendPresenceUpdate('recording', m.chat);
+    await m.reply(`*ايس كريم؟!ههههههههه، بهظر اتفضل*`);
     m.conn.sendMessage(m.chat, {audio: {url: vn}, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
   }
   return !0;
