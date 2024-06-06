191a6945636673d656d6c4b6f549177697c3f91a
Salahaldeen0saif <salahaldeensaifallah@gmail.com>
2024-06-06 19:16:10 +0300

    Update اوامر.js

--- plugins/اوامر.js
+++ plugins/اوامر.js
@@ -183,7 +183,7 @@ conn.sendMessage(m.chat, {
 }; 
 handler.help = ['main']
 handler.tags = ['group']
-handler.command = [`'كايدو','اوامر','ايس'] 
+handler.command = ['ايس|كايدو|اوامر'] 
 
 export default handler
 function clockString(ms) {
