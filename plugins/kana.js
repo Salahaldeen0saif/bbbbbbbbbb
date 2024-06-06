28b5a9c9eac738019b4f7d282ea03579ba243183
Salahaldeen0saif <salahaldeensaifallah@gmail.com>
2024-06-06 08:42:05 +0300

    Update kana.js

--- plugins/kana.js
+++ plugins/kana.js
@@ -12,16 +12,4 @@ handler.all = async function(m) {
 };
 export default handler;
 
-import fs from 'fs';
-const handler = (m) => m;
-handler.all = async function(m) {
-  const vn = './media/ace.mp3';
-  const chat = global.db.data.chats[m.chat];
-  if (/^ايس$/i.test(m.text) && !chat.isBanned) {
-    conn.sendPresenceUpdate('recording', m.chat);
-    await m.reply(`*ايس كريم؟!ههههههههه، بهظر اتفضل*`);
-    m.conn.sendMessage(m.chat, {audio: {url: vn}, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
-  }
-  return !0;
-};
-export default handler;
+
