0fb63faf37fc3835f51f45b0b4194dd04cbcef8f
Salahaldeen0saif <salahaldeensaifallah@gmail.com>
2024-06-08 21:40:42 +0300

    Update ترقيه.js

--- plugins/ترقيه.js
+++ plugins/ترقيه.js
@@ -27,8 +27,7 @@ handler.help = ['*201147144354xxx*','*@اسم المستخدم*','*محادثة 
 handler.tags = ['group']
 handler.command = /^(ترقيه|ترقية)$/i
 handler.group = true
-handler.rowner = true
-handler.botAdmin = true
 handler.Admin = true
+handler.botAdmin = true
 handler.fail = null
 export default handler
