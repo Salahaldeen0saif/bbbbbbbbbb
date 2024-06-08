c6b7ecdf35b51872705f163e292c3db8d0bc924e
Salahaldeen0saif <salahaldeensaifallah@gmail.com>
2024-06-08 20:18:06 +0300

    Update طرد.js

--- plugins/طرد.js
+++ plugins/طرد.js
@@ -7,7 +7,7 @@ if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: 
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 let owr = m.chat.split`-`[0]
 await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-m.reply(`*طردك فحلك روح ابكي عند امك ترجعك 😲!*`) 
+m.reply(`*سلملي على مادارا ههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههه @user *`) 
 
 }
 
