a4f3e1b2d78bee49159d58d3257f58fa363aaa07
Salahaldeen0saif <salahaldeensaifallah@gmail.com>
2024-06-08 19:39:00 +0300

    Create استيكفيد.js

---
+++ plugins/استيكفيد.js
@@ -0,0 +1,47 @@
+-
+cmd({
+            pattern: "sticker",
+            alias: ["s","س"],
+            desc: "Makes sticker of replied image/video.",
+            category: "group",
+            use: '<reply to any image/video.>',
+        },
+        async(Void, citel, text) => {
+            if (!citel.quoted) return citel.reply(`*֎╎رد  عـلـى صـورة*`);
+            let mime = citel.quoted.mtype
+            pack = Config.packname
+            author = Config.author
+            if (citel.quoted) {
+                let media = await citel.quoted.download();
+                citel.reply("*֎╎لـحـظـه بـرسـلـك الـمـلـصـق...*");
+                let sticker = new Sticker(media, {
+                    pack: pack, // The pack name
+                    author: author, // The author name
+                    type: text.includes("--crop" || '-c') ? StickerTypes.CROPPED : StickerTypes.FULL,
+                    categories: ["🤩", "🎉"], // The sticker category
+                    id: "12345", // The sticker id
+                    quality: 75, // The quality of the output file
+                    background: "transparent", // The sticker background color (only for full stickers)
+                });
+                const buffer = await sticker.toBuffer();
+                return Void.sendMessage(citel.chat, {sticker: buffer}, {quoted: citel });
+            } else if (/video/.test(mime)) {
+                if ((quoted.msg || citel.quoted)
+                    .seconds > 20) return citel.reply("*֎╎الـحـد الاقـصـى للـفـيـديـو 20 ثـانـيـه*");
+                let media = await quoted.download();
+                let sticker = new Sticker(media, {
+                    pack: pack, // The pack name
+                    author: author, // The author name
+                    type: StickerTypes.FULL, // The sticker type
+                    categories: ["🤩", "🎉"], // The sticker category
+                    id: "12345", // The sticker id
+                    quality: 70, // The quality of the output file
+                    background: "transparent", // The sticker background color (only for full stickers)
+                });
+                const stikk = await sticker.toBuffer();
+                return Void.sendMessage(citel.chat, {  sticker: stikk   }, {    quoted: citel });
+            } else {
+                citel.reply("*֎╎رد  عـلـى صـورة*");
+            }
+        }
+    )
