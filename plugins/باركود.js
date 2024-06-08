82be677435493e5881b2e2f48a738e0bf5cfbc8e
Salahaldeen0saif <salahaldeensaifallah@gmail.com>
2024-06-08 06:38:32 +0300

    Update باركود.js

--- plugins/باركود.js
+++ plugins/باركود.js
@@ -1,7 +1,7 @@
 import { toDataURL } from 'qrcode'
 let handler = async (m, { text, conn }) => {
 if (!text) throw `*اكتب كلام الي تبغاه يحول الى باركود 🧸*`
-conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', '¯\\_(BY:HODA_AND_MAHDI', m)
+conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', '*_『كرزة⊰🍒⊱بوت』_*', m)
 }
 handler.help = ['', 'code'].map(v => 'qr' + v + ' <teks>')
 handler.tags = ['tools']
