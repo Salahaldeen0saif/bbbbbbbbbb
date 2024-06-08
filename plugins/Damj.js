ab13d351bfd2dab3777137a3f78b1141bbabfdfb
Salahaldeen0saif <salahaldeensaifallah@gmail.com>
2024-06-08 06:35:11 +0300

    Update Damj.js

--- plugins/Damj.js
+++ plugins/Damj.js
@@ -1,24 +1,30 @@
-import { sticker } from '../lib/sticker.js'
-import MessageType from '@adiwajshing/baileys'
 import fetch from 'node-fetch'
-import fs from "fs"
-let handler = async (m, { conn, text, args }) => {
-if (!args[0]) throw '*باستخدام هذا الأمريجب ان يكون #دمج <ايموجي 1>+<ايموجي 2>*\n*مثال:*\n*.دمج 🤨+😣*'
-let [emoji1, emoji2] = text.split`+`
-let anu = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`)
-for (let res of anu.results) {
-let stiker = await sticker(false, res.url, global.packname, global.author)
-conn.sendFile(m.chat, stiker, null, { asSticker: true })
-}}
-handler.help = ['دمج'].map(v => v + ' emot1|emot2>')
-handler.tags = ['fun']
-handler.command = /^(دمج)$/i
-export default handler
+import { sticker } from '../lib/sticker.js'
+
 const fetchJson = (url, options) => new Promise(async (resolve, reject) => {
-fetch(url, options)
-.then(response => response.json())
-.then(json => {
-resolve(json)
+  fetch(url, options)
+    .then(response => response.json())
+    .then(json => {
+      resolve(json)
+    })
+    .catch((err) => {
+      reject(err)
+    })
 })
-.catch((err) => {
-reject(err)
+
+let handler = async (m, { conn, text, args, usedPrefix, command }) => {
+  if (!args[0]) throw `📌 مثال: ${usedPrefix + command} 😎+🤑`
+  if (!text.includes('+')) throw `✳️ قم بفصل الإيموجي بواسطة *+*\n\n📌 مثال:\n*${usedPrefix + command}* 😎+🤑`
+  let [emoji, emoji2] = text.split`+`
+  let anu = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=YOUR_API_KEY&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji)}_${encodeURIComponent(emoji2)}`)
+  for (let res of anu.results) {
+    let sticker = await sticker(false, res.url, global.packname, global.author)
+    conn.sendFile(m.chat, sticker, null, { asSticker: true }, m)
+  }
+}
+
+handler.help = ['emojimix <emoji+emoji>']
+handler.tags = ['sticker']
+handler.command = ['emojimix','دمج']
+
+export default handler
