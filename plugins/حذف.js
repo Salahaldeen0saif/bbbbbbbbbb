d6c548ec9062bd4dd2454f3496e50d19587d46da
Salahaldeen0saif <salahaldeensaifallah@gmail.com>
2024-06-10 19:27:53 +0300

    Update حذف.js

--- plugins/حذف.js
+++ plugins/حذف.js
@@ -1,21 +1,16 @@
-let handler = async (m, { conn, usedPrefix, command }) => {
-    if (!m.quoted) throw `✳️ قم بالرد على الرسالة التي تريد حذفها`
-    
-    try {
-      let delet = m.message.extendedTextMessage.contextInfo.participant
-      let bang = m.message.extendedTextMessage.contextInfo.stanzaId
-      return conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: true, id: bang, participant: delet } })
-    } catch {
-      return conn.sendMessage(m.chat, { delete: m.quoted.vM.key })
-    }
-  }
-  
-  handler.help = ['delete']
-  handler.tags = ['group']
-  handler.command = ['del','delete','حذف']
-  handler.group = false
-  handler.admin = true
-  handler.botAdmin = true
-  
-  export default handler
-  
+let handler = async (m, { conn, usedPrefix, command }) => {	
+if (!m.quoted) throw `*❮ ❗┇يجب ان تضع ريبلاي للرسالة الذي تريد حذفها❯*`
+try {
+let delet = m.message.extendedTextMessage.contextInfo.participant
+let bang = m.message.extendedTextMessage.contextInfo.stanzaId
+return conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: true, id: bang, participant: delet }})
+} catch {
+return conn.sendMessage(m.chat, { delete: m.quoted.vM.key })
+}}
+handler.help = ['del', 'delete']
+handler.tags = ['group']
+handler.command = /^حذف|احذف?$/i
+handler.group = true
+handler.admin = true
+handler.botAdmin = true
+export default handler
