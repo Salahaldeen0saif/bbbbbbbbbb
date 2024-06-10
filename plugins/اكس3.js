2a4548a58cce57ec86ee74888ceb0e532a344b44
Salahaldeen0saif <salahaldeensaifallah@gmail.com>
2024-06-10 20:21:52 +0300

    Update اكس3.js

--- plugins/اكس3.js
+++ plugins/اكس3.js
@@ -1,12 +1,16 @@
+import MessageType from '@whiskeysockets/baileys';
 
-let handler = async (m, { conn, text }) => {
-  let room = Object.values(conn.game).find(room => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender))
-if (room == undefined) return conn.reply(m.chat,`فين الرساله الي عايز تمسحها!`, m)
-delete conn.game[room.id]
-await conn.reply(m.chat, `تم الحذف`, m)
-}
-handler.help = ['delttt']
-handler.tags = ['game']
-handler.command = ['كنسل', 'delttt', 'delxo']
+const handler = async (m, {conn, usedPrefix, command}) => {
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
+  const tradutor = _translate.plugins.game_delttt
 
-export default handler
+  const room = Object.values(conn.game).find((room) => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender));
+  if (room == undefined) return conn.sendButton(m.chat, tradutor.texto1, wm, null, [[tradutor.texto2, `${usedPrefix}ttt ${tradutor.texto3}`]], m);
+  delete conn.game[room.id];
+  await m.reply(tradutor.texto4);
+};
+handler.command = /^(delttt|deltt|delxo|كنسل)$/i;
+handler.fail = null;
+export default handler;
