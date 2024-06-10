ec74522cf67ec42647150379cdbba28e6573494c
Salahaldeen0saif <salahaldeensaifallah@gmail.com>
2024-06-10 19:28:47 +0300

    Update اكس3.js

--- plugins/اكس3.js
+++ plugins/اكس3.js
@@ -1,9 +1,12 @@
-import MessageType from '@adiwajshing/baileys'
-let handler = async (m, { conn, usedPrefix, command }) => {
-let room = Object.values(conn.game).find(room => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender))
-if (room == undefined) return conn.sendButton(m.chat, '*[❗] انت لست في لعبه تيك تاك تو (اكس او)*', wm, null, [['ابدأ غرفه جديدة', `${usedPrefix}ttt مباراه جديدة`]], m)
+
+let handler = async (m, { conn, text }) => {
+  let room = Object.values(conn.game).find(room => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender))
+if (room == undefined) return conn.reply(m.chat,`فين الرساله الي عايز تمسحها!`, m)
 delete conn.game[room.id]
-await m.reply('*[ ✔ ] تمت ازاله الغرفه*')}
-handler.command = /^(delttt|deltt|delxo|كنسل)$/i
-handler.fail = null
+await conn.reply(m.chat, `تم الحذف`, m)
+}
+handler.help = ['delttt']
+handler.tags = ['game']
+handler.command = ['كنسل', 'delttt', 'delxo']
+
 export default handler
