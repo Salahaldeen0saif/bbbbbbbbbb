03b8bc89d7ff4d0fef7408937e8ac9521bd806e8
Salahaldeen0saif <salahaldeensaifallah@gmail.com>
2024-06-10 20:39:32 +0300

    Update اكس3.js

--- plugins/اكس3.js
+++ plugins/اكس3.js
@@ -1,9 +1,9 @@
 import MessageType from '@whiskeysockets/baileys'
 let handler = async (m, { conn, usedPrefix, command }) => {
 let room = Object.values(conn.game).find(room => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender))
-if (room == undefined) return conn.reply(m.chat, `أنت لست في أي لعبة \n\nلبدء غرفة جديدة, يمكنك استخدام: ${usedPrefix} مبارة جديدة`, m)
+if (room == undefined) return conn.reply(m.chat, `*أنت لست في أي لعبة* \n\n_لبدء غرفة جديدة, يمكنك استخدام: ${usedPrefix} اكس او_`, m)
 delete conn.game[room.id]
-await m.reply('[ ✔ ] _تمت ازاله الغرفة_')}
+await m.reply('*[ ✔ ] _تمت ازاله الغرفة_*')}
 handler.command = /^(delttt|deltt|delxo|كنسل)$/i
 handler.fail = null
 export default handler
